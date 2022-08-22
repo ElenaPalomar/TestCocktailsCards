5093d0c008dbd82be36e538de68a7c5ba37b9e81
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-08-22 23:54:56 +0200

    change hover for click styles

---
+++ script.js
@@ -0,0 +1,36 @@
+// VARIABLES
+const card = document.querySelector('.js_card');
+
+const title = document.querySelector('.js_title');
+
+const link = document.querySelector('.js_link');
+
+const cardContent = document.querySelector('.js_cardContent');
+
+
+// FUNCTIONS
+
+
+
+// HANDLE FUNCTIONS
+
+const handleClickCard = () => {
+
+  card.classList.toggle('card');
+  card.classList.toggle('card--open');
+
+  title.classList.toggle('title');
+  title.classList.toggle('title--open');
+
+  link.classList.toggle('link');
+  link.classList.toggle('link--open');
+
+  cardContent.classList.toggle('hidden');
+
+};
+
+
+
+// LISTENERS
+
+card.addEventListener('click', handleClickCard);
